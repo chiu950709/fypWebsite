4128d1232262373d8ecdbe2e565e54b8dea2ed9f
LeungRaymond <>
2019-03-09 20:38:21 +0800

    Web Compiler

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var fs = require('file-system');
 
 var url = "mongodb+srv://dbadmin:dbadmin@cluster0-fruhy.mongodb.net/test?retryWrites=true"
 /* GET home page. */
@@ -44,6 +45,7 @@ router.post('/insert', function(req, res){
 		}
 	});
 });
+
 function insert(query, callback){
 	mongodb.connect(url, function(err, client){
 		console.log("Connected");
@@ -52,4 +54,62 @@ function insert(query, callback){
 		});
 	});
 }
+
+//compile
+router.post('/compile', function(req, res){
+	var code = req.body.code;
+	var output;
+	var output;
+	console.log(code);
+	fs.writeFile('Temp.java', code, function (err) {
+	  if (err) throw err;
+	  console.log('File Saved!');
+	  compile_temp(function(response){
+	  	console.log(response);
+		res.setHeader("content-type","text/html");
+		res.send(response);
+	  	});
+	});
+});
+
+function compile_temp(callback){
+	var process = require('child_process');
+	var astr = "";
+	var bstr = "";
+
+	var options={encoding:'utf8'};
+	var javac  = process.spawn('javac', ['Temp.java'],options);
+
+	javac.stderr.on('data', function (data) {
+	  console.log('stderr: ' + data);
+	});
+
+	javac.on('close', function (code) {
+	  console.log('child process exited with code ' + code);
+	});
+
+	console.log('Spawned child pid: ' + javac.pid);
+
+	var java  = process.spawn('java', ['Temp'],options);
+	var response = {};
+
+	java.stdout.on('data', function(data) {
+	    astr = astr + data.toString();
+	    response.out = astr;
+	});
+
+	java.stderr.on("data", function (data) {
+		bstr = bstr + data.toString();
+		response.err = astr;
+	});
+
+	java.once('close', function(){
+	   callback(response);
+	});
+
+	
+}
+
+
+
 module.exports = router;
